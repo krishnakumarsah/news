2434b828349a999e743eb7c7781cfba6d75d9f9a
Krishna Kumar Sah <krishnakumarsah91@gmail.com>
2020-06-06 16:35:29 +0545

    file changes

--- is-function.php
+++ is-function.php
@@ -3,7 +3,7 @@ $var = 500;
 // echo is_int($var);
 var_dump(is_int($var)) . "<br>";
 if (is_int($var)) {
-	echo "<br> $var is integer.Mr.amiri";
+	echo "<br> $var is integer Mr.Krishna Kumar Sah";
 }else{
 	echo "<br> $var ghhghghbjkjk;
 }
